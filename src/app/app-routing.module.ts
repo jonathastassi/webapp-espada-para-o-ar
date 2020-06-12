535993af26d88aa27451d705a5dc7b778f071b92
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2020-06-12 09:00:10 -0300

    App componente e module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  { path: 'pre-game', loadChildren: () => import('./pages/pre-game/pre-game.module').then(m => m.PreGameModule) },
+  { path: 'game', loadChildren: () => import('./pages/game/game.module').then(m => m.GameModule) },
+  { path: 'pos-game', loadChildren: () => import('./pages/pos-game/pos-game.module').then(m => m.PosGameModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ComponentsModule } from './components/components.module';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './pages/home/home.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/game.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    MaterialModule,
+    ComponentsModule,
+    StoreModule.forRoot({ game: reducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
